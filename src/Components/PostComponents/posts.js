8a8f2ae661bdb46c67acc1a89c66fd43dd2590d2
mushfiqur06 <mushfiqur06@gmail.com>
2018-07-31 02:23:52 +0600

    Fix Blog column

--- src/Components/PostComponents/posts.js
+++ src/Components/PostComponents/posts.js
@@ -4,10 +4,19 @@ import Post from './SinglePost/post'
 class Posts extends Component{
     render(){
         return(
-            <div className=''>
-                {this.props.posts.map( (post, index) => {
-                    return <Post key={index} post={post} />
-                })}
+            <div className='col-md-4'>
+                <div className='blogWrapper'>
+                    <div className='postThumbnail'>
+                        <img src={this.props.post.img} />
+                    </div>
+                  <div className='blogHeader'>
+                    <h2>
+                        <a href='#'>{this.props.post.title}</a>
+                    </h2>
+                    <p>{this.props.post.desc}</p>
+                  </div>
+                </div>
+                
             </div>
         )
     }
