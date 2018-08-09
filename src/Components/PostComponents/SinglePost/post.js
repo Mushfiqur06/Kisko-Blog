d7b720f69da12acc7fb9819d96a60926242bd0af
mushfiqur06 <mushfiqur06@gmail.com>
2018-08-09 14:03:52 +0600

    Some modified post.js file

--- src/Components/PostComponents/SinglePost/post.js
+++ src/Components/PostComponents/SinglePost/post.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const Post = (props) => {
+
     return (
         <div>
+            <img src={this.props.post.img} />
             <h2><a href="#">{props.post.title}</a></h2>
             <p>{props.post.desc}</p>
         </div>
