2f5db81b8355550a27f2b1879a0e93224f9d38dc
mushfiqur06 <mushfiqur06@gmail.com>
2018-08-30 00:09:08 +0600

    Some Modified posts.js file

--- src/Components/PostComponents/posts.js
+++ src/Components/PostComponents/posts.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import Post from './SinglePost/post'
-import BlogImgOne from './../../assets/img/img1.jpg'
 
 class Posts extends Component{
     render(){
+        // let blogImagesArray = ['img1', 'img2', 'img3'];
+        // let blogImages = blogImagesArray.map((image,index) => {
+        //     return <img key={index} src={require(`./../../assets/img/${image}.jpg`)} alt="" className="img-responsive" />
+        // })
         return(
             <div className='col-md-4'>
                 <div className='blogWrapper'>
                     <div className='postThumbnail'>
-                        <img src={BlogImgOne} />
+                        <img src={this.props.post.img} />
                     </div>
                   <div className='blogHeader'>
                     <h2>
